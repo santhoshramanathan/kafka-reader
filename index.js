aa9ba640c1fcb1f0d3d736595aeb0444b833cd17
Shilpa S <shilpa.11@tcs.com>
2020-08-14 11:22:58 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const fetch = require("node-fetch");
+const kafka = require("kafka-node");
+
+const middleware_url = process.env.MIDDLEWARE_URL;
+
+const Consumer = kafka.Consumer,
+  // The client specifies the ip of the Kafka producer and uses
+  // the zookeeper port 2181
+  client = new kafka.KafkaClient("172.21.202.112:2181"),
+  // The consumer object specifies the client and topic(s) it subscribes to
+  consumer = new Consumer(
+    client,
+    [{ topic: "dbserver1.inventory.customers", partition: 0 }],
+    {
+      autoCommit: false,
+    }
+  );
+
+consumer.on("message", async function (message) {
+  // grab the main content from the Kafka message
+  var data = JSON.parse(message.value);
+  console.log(data);
+  // const result = await fetch(`${middleware_url}/book`, {});
+  // console.log("DB update result...", result);
+});
